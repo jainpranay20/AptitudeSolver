ee55246d71bfa1ace996119db511c6d7b18e32f6
jainpranay20 <jainpranay20@gmail.com>
2022-11-24 23:56:58 +0530

    new apti functions added

--- src/main/java/org/example/AptitudeFunction.java
+++ src/main/java/org/example/AptitudeFunction.java
@@ -121,4 +121,158 @@ public class AptitudeFunction
         }
     }
 
-}
+    public String simpleInterest(double principal, double rate, double time)
+    {
+        try {
+            if (principal == 0 || rate==0 || time==0) {
+                throw new Exception();
+            }
+            double si = (principal * rate * time)/ 100;
+            return Double.toString(si);
+        }catch (Exception e)
+        {
+            return "Invalid original value";
+        }
+    }
+
+    public String compoundInterest(double principal, double rate, double time)
+    {
+        String result;
+        try {
+            if (principal == 0 || rate==0 || time==0) {
+                throw new Exception();
+            }
+            double amount = principal * (Math.pow((1 + rate/100), (time)));
+            double interest = amount - principal;
+            return result = "Amount is" + amount + "Interest is" + interest;
+        }catch (Exception e)
+        {
+            return "Invalid original value";
+
+        }
+    }
+
+    public String compareProfitLoss(double costPrice,double sellingPrice)
+    {
+        String res;
+        try {
+            if (costPrice == 0) {
+                throw new Exception();
+            }
+            else if(costPrice-sellingPrice>0) {
+                double ans=costPrice-sellingPrice;
+                double res1=(ans/costPrice)*100;
+                res="loss" + ans + "loss percent" +res1;
+            }
+            else if(costPrice-sellingPrice<0) {
+                double ans=costPrice-sellingPrice;
+                double res1=(-1 * ans/costPrice)*100;
+                res="profit"+ ans +"profit percent" +res1;
+            }
+            else {
+                res="neutral";
+            }
+            return res;
+        }
+        catch (Exception e)
+        {
+            return "Invalid original value";
+        }
+    }
+
+
+    public String distanceSpeedAndTime(double speed,double time)
+    {
+        String res;
+        try {
+            if (speed == 0 || time == 0) {
+                throw new Exception();
+            }
+            double distance = speed * time;
+            res= "Distance is "+distance;
+            return res;
+        }
+        catch (Exception e)
+        {
+            return "Invalid original Value";
+        }
+    }
+
+    public double squareArea(double side) {
+        double area = side*side;
+        return area;
+    }
+
+    public double rectangleArea(double length, double breadth) {
+        double area = length*breadth;
+        return area;
+    }
+
+    public double circleArea(double radius) {
+        double pi = 3.14;
+        double area = pi*radius*radius;
+        return area;
+    }
+
+    public double triangleArea(double breadth,double height) {
+        double area = 0.5*breadth*height;
+        return area;
+    }
+
+    public double surfaceAreaOfCube(double side) {
+        double area = 6*side*side;
+        return area;
+    }
+    public double volumeOfCube(double side) {
+        double volume = side*side*side;
+        return volume;
+    }
+
+
+    public double surfaceAreaOfCuboid(double length,double breadth,double height) {
+        double area = 2*(length*breadth+breadth*height+height*length);
+        return area;
+    }
+    public double volumeOfCuboid(double length,double breadth,double height) {
+        double volume = length*breadth*height;
+        return volume;
+    }
+
+
+    public double surfaceAreaOfCylinder(double radius,double height) {
+        double pi = 3.14;
+        double area = 2*pi*radius*(radius+height);
+        return area;
+    }
+
+    public double volumeOfCylinder(double radius,double height) {
+        double pi = 3.14;
+        double volume = pi*radius*radius*height;
+        return volume;
+    }
+
+    public double surfaceAreaOfCone(double radius,double length) {
+        double pi = 3.14;
+        double area = pi*radius*(radius+length);
+        return area;
+    }
+    public double volumeOfCone(double radius, double height) {
+        double pi = 3.14;
+        double volume = 1/3*pi*radius*radius*height;
+        return volume;
+    }
+
+
+    public double surfaceAreaOfSphere(double radius) {
+        double pi = 3.14;
+        double area = 4*pi*radius*radius;
+        return area;
+    }
+    public double volumeOfSphere(double radius) {
+        double pi = 3.14;
+        double volume = 4/3*pi*radius*radius*radius;
+        return volume;
+
+
+
+    }
